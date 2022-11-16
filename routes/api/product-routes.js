a83f9e51406dfd1c1f3a923382ede3d998eb8aed
Haleigh Rzonca <hgielah@gmail.com>
2022-11-15 16:45:47 -0800

    finally got the app to work! changed sequelize.sync to false

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,14 +1,13 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-
-
 // get all products
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag }],
+      include: [{ model: Category ,  model: Tag }],
     });
+    console.log(productData);
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
